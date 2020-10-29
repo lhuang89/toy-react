33e7cc9b2dbbfe0c6e26fe3c84cc7818c4bec2b0
lhuang89 <lhuang89@gmail.com>
2020-10-28 23:22:10 -0500

    Updated code to allow re-render

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,4 @@
+const RENDER_TO_DOM = Symbol("render to dom");
 
 class ElementWrapper {
     constructor(type){
@@ -9,7 +10,15 @@ class ElementWrapper {
     }
 
     appendChild(component){
-        this.root.appendChild(component.root);
+        let range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        component[RENDER_TO_DOM](range);
+    }
+
+    [RENDER_TO_DOM](range){
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 
 
@@ -18,6 +27,11 @@ class ElementWrapper {
 class TextWrapper {
     constructor(content){
         this.root = document.createTextNode(content);
+    }
+
+    [RENDER_TO_DOM](range){
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -34,16 +48,18 @@ export class Component {
         this.children.push(component);
     }
 
-    get root(){
-        if (!this._root){
-            this._root = this.render().root;
-        }
-        return this._root;
+    [RENDER_TO_DOM](range){
+        this.render()[RENDER_TO_DOM](range);
     }
+
 }
 
 export function render(compoment, parentElement){
-    parentElement.appendChild(compoment.root);
+    let range=document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    compoment[RENDER_TO_DOM](range);
 }
 
 export function createElement(type, attributes, ...children){
